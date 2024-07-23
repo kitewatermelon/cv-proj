0150a01b22b7994cc56910823456692068a41b83
YSPark <parkite576@gmail.com>
2024-07-23 22:11:26 +0900

    commit

---
+++ yolov7.py
@@ -0,0 +1,3 @@
+# r'''
+# python C:\Users\Administrator\Desktop\cv-proj\yolov7\train.py --img 416 --batch 16 --epochs 50 --data C:\Users\Administrator\Desktop\cv-proj\dataset\data.yaml  --weights 'yolov7.pt'
+# '''
